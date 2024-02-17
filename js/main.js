a546c18c20a2dbd950c6f6e7daa9c7b0798e0d61
iCRAYO <madeinonline@gmail.com>
2024-02-17 10:38:29 +0000

    Sticky header

--- js/main.js
+++ js/main.js
@@ -129,4 +129,18 @@ sr.reveal('.footer__item', {
     origin: 'bottom',
 });
 
-// end scroll reveal animations
+/* ========================================== 
+scrollTop() >= 300
+Should be equal the the height of the header
+========================================== */
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
